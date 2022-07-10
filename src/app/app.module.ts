602cad75de3660553a676c8ba63804b33ab26f08
mateen777 <md.mateen42@gmail.com>
2022-07-10 19:52:17 +0530

    setup primeng and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import {CardModule} from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import {AccordionModule} from 'primeng/accordion';
 
 @NgModule({
   declarations: [
@@ -11,8 +16,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    CardModule,
+    ButtonModule,
+    AccordionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
